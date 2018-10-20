83dae8eacbf55c67a675075be3c6011341e7293a
jielyugt <jie.lyu@gatech.edu>
2018-10-20 14:24:33 -0400

    button function added

---
+++ v1/Assets/focusButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class focusButton : MonoBehaviour {
+    public GameObject button1;
+    public GameObject button2;
+
+    public void hitFocus () {
+        button1.SetActive(false);
+        button2.SetActive(false);
+
+    }
+
+
+
+    // Update is called once per frame
+
+}
